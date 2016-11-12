5646226ef1a2de62346a712690dd5ae89d73246d
Arcanehood <Arcanehood@users.noreply.github.com>
2016-11-12 14:35:49 -0800

    controls.js; removed unnecessary things.

--- controls.js
+++ controls.js
@@ -61,7 +61,7 @@ document.onmousemove = function(mouse) {
 };
 
 document.onclick = function(mouse) {
-    if (writing === false && gameStart && player.hp > 0) {
+    if (!writing&&gameStart&&player.hp>0) {
         player.doAttack();
         console.log(player.aimangle);
     }
